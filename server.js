c4221764c6cd129db599052c6c591808bbeee7dd
aman <harshparamar84@gmail.com>
2023-02-03 20:54:48 +0530

    fix error

--- server.js
+++ server.js
@@ -1,56 +1,56 @@
-import DHT from "@hyperswarm/dht"
-import readyFiles from "./utils.js"
-import md5 from "md5"
-import express from "express"
-import { basename } from "path"
-import pump from "pump"
-import net from "net"
+import DHT from "@hyperswarm/dht";
+import readyFiles from "./utils.js";
+import md5 from "md5";
+import express from "express";
+import { basename } from "path";
+import pump from "pump";
+import net from "net";
 
-const data = {}
+const data = {};
 
 export default async function (files) {
-  const port = 8080
-
+  const port = 8080;
   readyFiles(files).forEach((t) => {
-    data[md5(t)] = t
-  })
+    data[md5(t)] = t;
+  });
 
-  console.log("File Selected :")
+  console.log(data);
+  console.log("File Selected :");
   console.log(
     Object.values(data)
       .map((t) => basename(t))
       .join("\n")
-  )
-  console.log()
+  );
+  console.log();
 
-  readyServer(port)
+  readyServer(port);
 
-  const node = new DHT()
-  const server = node.createServer()
+  const node = new DHT();
+  const server = node.createServer();
 
   server.on("connection", async function (noiseSocket) {
-    pump(noiseSocket, net.connect(port, "localhost"), noiseSocket)
-  })
+    pump(noiseSocket, net.connect(port, "localhost"), noiseSocket);
+  });
 
-  const keyPair = DHT.keyPair()
+  const keyPair = DHT.keyPair();
 
-  console.log("Key :", keyPair.publicKey.toString("hex"))
+  console.log("Key :", keyPair.publicKey.toString("hex"));
 
-  await server.listen(keyPair)
+  await server.listen(keyPair);
 }
 
 function readyServer(port) {
-  const app = express()
+  const app = express();
   app.get("/", (req, res) => {
-    res.json(Object.keys(data))
-  })
+    res.json(Object.keys(data));
+  });
 
   app.get("/:hash", (req, res) => {
-    const { hash } = req.params
-    res.download(data[hash])
-  })
+    const { hash } = req.params;
+    res.download(data[hash]);
+  });
 
   app.listen(port, () => {
-    console.log("Express server Listening on " + port)
-  })
+    console.log("Express server Listening on " + port);
+  });
 }
--- utils.js
+++ utils.js
@@ -1,31 +1,35 @@
-import fs from "fs"
-import path from "path"
+import fs from "fs";
+import path from "path";
+
 export default function readyFiles(fileList) {
-  const files = []
-  fileList = fileList.map((file) => ({ name: file, isDir: fs.statSync(file).isDirectory() }))
+  const files = [];
+  fileList = fileList.map((file) => ({
+    name: file,
+    isDir: fs.statSync(file).isDirectory(),
+  }));
   fileList.forEach((file) => {
     if (file.isDir) {
-      const f = getFiles(file.name)
-      files.push(...f)
+      const f = getFiles(file.name);
+      files.push(...f);
     } else {
-      files.push({ file: file.name, dirname: path.dirname(file.name) })
+      files.push(file.name);
     }
-  })
+  });
 
-  return files
+  return files;
 }
 
 function getFiles(dir) {
-  const files = getFilesHelper(dir)
-  return files
+  const files = getFilesHelper(dir);
+  return files;
 }
 
 function getFilesHelper(dir) {
   return fs.readdirSync(dir).flatMap((item) => {
-    const p = `${dir}${path.sep}${item}`
+    const p = `${dir}${path.sep}${item}`;
     if (fs.statSync(p).isDirectory()) {
-      return getFilesHelper(p)
+      return getFilesHelper(p);
     }
-    return p
-  })
+    return p;
+  });
 }
